cade22f07f91b62d63f08b51f24f1fa441bb5fed
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-03-31 23:23:19 -0400

    Update F1 22.py

--- F1 22.py
+++ F1 22.py
@@ -12,36 +12,19 @@ if starting:
     
     v = vJoy[0]
     v.x, v.y, v.z, v.rx, v.ry, v.rz, v.slider, v.dial = (int32_min,) * 8
-    # =============================================================================================
-    # //////////////////////////////////////// SETTINGS ///////////////////////////////////////////
-    # =============================================================================================
-    # Mouse settings - Change the sensitivity here
-    # =============================================================================================
     global mouse_sensitivity, mouse_throttle_sensitivity
-    mouse_throttle_sensitivity = 50
-    mouse_sensitivity = 10
-    # Additional controls: Wheel Up = Reset Steering to Center @ Line 89
-    # =============================================================================================
-    # Throttle settings
-    # Init values, do not change
+    mouse_throttle_sensitivity = 50 #This can be altered for input: Throttle Sensitivity
+    mouse_sensitivity = 10 #This can be altered for input: Steering Sensitivity
     global throttle, throttle_max, throttle_min
     throttle_max = int32_max
     throttle_min = int32_min
     throttle = throttle_min 
-    # =============================================================================================
-    # Steering settings
-    # =============================================================================================
     global steering, steering_max, steering_min, steering_center_reduction    
     # Init values, do not change
     steering = 0.0
     steering_max = float(int32_max)
     steering_min = float(int32_min)
     steering_center_reduction = 1.0
-# =================================================================================================
-# LOOP START
-# =================================================================================================
-# Steering logic
-# =================================================================================================
 if mouse.wheelUp:
 	steering = 0.0
 	throttle = 0
@@ -54,24 +37,14 @@ if steering > steering_max:
 elif steering < steering_min:
     steering = steering_min
 v.x = int(round(steering))
-# =================================================================================================
-# Throttle logic and Braking logic
-# =================================================================================================
 throttle = throttle + (float(-mouse.deltaY) * mouse_throttle_sensitivity)
 if throttle > throttle_max:
     throttle = throttle_max
 elif throttle < steering_min:
     throttle = throttle_min
 v.y = int(round(throttle))
-# =================================================================================================
-# vJoy BUTTONS 
-# F1 2022 allows keyboard controls mixed with other input devices, so we don't need to set any more
-# =================================================================================================
 v.setButton(0,int(mouse.leftButton))
 v.setButton(1,int(mouse.rightButton))
 v.setButton(2,int(mouse.middleButton))
-# =================================================================================================
-# PIE diagnostics logic
-# =================================================================================================
 diagnostics.watch(v.x)
 diagnostics.watch(v.y)
